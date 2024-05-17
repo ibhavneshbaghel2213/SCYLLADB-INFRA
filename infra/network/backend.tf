1135f063088609f8ce61aeff2979f66e98b8fc7f
Bhavnesh Baghel <dazzlingbhavnesh@gmail.com>
2024-05-17 23:55:12 +0530

    region ap-south

--- infra/network/backend.tf
+++ infra/network/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-tool-scylladb-new"
-    key    = "my-tool-scylla-new/network.tfstate"
+    bucket = "my-scylladb-new"
+    key    = "my-scylladb-new/network.tfstate"
     region = "ap-south-1"
   }
 }
